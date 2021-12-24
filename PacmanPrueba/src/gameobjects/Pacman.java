5f4ad307ea69b81414558d187ce1862b1caed9a8
David <id11011@usal.es>
2021-12-24 14:17:27 +0100

    V1.0
    Graphics and Pacman Basics

---
+++ PacmanPrueba/src/gameobjects/Pacman.java
@@ -0,0 +1,55 @@
+package gameobjects;
+
+import supers.GameObject;
+
+public class Pacman extends GameObject{
+    int lives;
+    int rx, ry;
+    String stop;
+
+    public Pacman() {
+        super(168,168,0,0,4);
+        lives = 0;
+        rx = 0;
+        ry = 0;
+        stop = "C:\\Users\\David\\Desktop\\PacmanPrueba\\src\\images\\pacman.png";
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+
+    public int getRx() {
+        return rx;
+    }
+
+    public void setRx(int rx) {
+        this.rx = rx;
+    }
+
+    public int getRy() {
+        return ry;
+    }
+
+    public void setRy(int ry) {
+        this.ry = ry;
+    }
+
+    public String getStop() {
+        return stop;
+    }
+
+    public void setStop(String stop) {
+        this.stop = stop;
+    }
+    
+    
+    
+    
+    
+    
+}
